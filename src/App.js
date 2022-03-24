ee15837d9cb7ad271f50cf4c6db70e7e42203cfe
ktxdev <sean.ktxdev@gmail.com>
2022-03-24 19:07:42 +0200

    Create input field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('')
   return (
     <div className="App">
+      {/* Input */}
+      <input value={input} onChange={e => setInput(e.target.value)} />
+      {/* Button */}
+      <button>Send Message</button>
+      {/* Messages */}
     </div>
   );
 }
